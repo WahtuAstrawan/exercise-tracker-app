130c87741ff5be6cf286585ac0b48eddd02ec700
Wahtu Astrawan <chatt699@gmail.com>
2024-02-05 09:01:38 +0800

    adding rate limiter to api for security

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
     windowMs: 60 * 1000,
-    max: 5,
+    max: 100,
     message: "You have exceeded your 100 requests per minute limit",
     headers: true,
 });
